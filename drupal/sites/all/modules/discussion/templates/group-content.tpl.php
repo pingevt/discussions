6ab0b4ac48133687470ad3280810eea64f145ba7
Pete Inge <pete.inge@gmail.com>
2014-06-18 19:24:19 -0400

    - continued development
    Signed-off-by: Pete Inge <pete.inge@gmail.com>

--- drupal/sites/all/modules/discussion/templates/group-content.tpl.php
+++ drupal/sites/all/modules/discussion/templates/group-content.tpl.php
@@ -2,8 +2,6 @@
   <div class="col-md-3 sidebar">
     <?php
       if (isset($element['user'])) print render($element['user']);
-      if (isset($element['groups'])) print render($element['groups']);
-      if (isset($element['add_content'])) print render($element['add_content']);
     ?>
   </div>
   <div class="col-md-9">
