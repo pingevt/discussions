87fc513c6cafe529a6c1123a8948967d04b7af5f
Pete Inge <pete.inge@gmail.com>
2014-08-01 22:47:39 -0400

    - added galleries and events
    Signed-off-by: Pete Inge <pete.inge@gmail.com>

--- drupal/sites/all/modules/discussion/templates/mini-calendar.tpl.php
+++ drupal/sites/all/modules/discussion/templates/mini-calendar.tpl.php
@@ -2,15 +2,15 @@
   <script type="text/template" id="mini-calendar-template">
 
     <div class="row text-center">
-      <div class="col-md-2 clndr-prev">&lsaquo;</div>
-      <div class="col-md-10 month"><%= month %></div>
-      <div class="col-md-2 clndr-next">&rsaquo;</div>
+      <div class="col-xs-2 clndr-prev">&lsaquo;</div>
+      <div class="col-xs-10 month"><%= month %></div>
+      <div class="col-xs-2 clndr-next">&rsaquo;</div>
     </div>
     <div class="row text-center">
-      <% _.each(daysOfTheWeek, function(day) { %><div class="col-md-2"><%= day %></div><% }); %>
+      <% _.each(daysOfTheWeek, function(day) { %><div class="col-xs-2"><%= day %></div><% }); %>
     </div>
     <div class="row text-center">
-      <% _.each(days, function(day) { %><div class="col-md-2 <%= day.classes %>" id="<%= day.id %>"><%= day.day %></div><% }); %>
+      <% _.each(days, function(day) { %><div class="col-xs-2 <%= day.classes %>" id="<%= day.id %>"><%= day.day %></div><% }); %>
     </div>
 
 
@@ -20,7 +20,7 @@
   <h3>Calendar</h3>
 
   <div class="row text-center">
-    <div class="col-md-12 col-md-offset-1">
+    <div class="col-xs-12 col-md-offset-1">
       <div class="mini-calendar" <?php print drupal_attributes($element['#attributes']); ?>></div>
     </div>
   </div>
